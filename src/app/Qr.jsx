2aed25a65ba87d590b900d630fd8b21b13659efc
jojeongj <https://github.com/jojeongj>
2023-08-28 16:37:03 +0900

    qr코드 인식할 수 있도록

--- src/app/Qr.jsx
+++ src/app/Qr.jsx
@@ -1,14 +1,33 @@
+import { useState } from "react";
 import Layout from "../components/Layout";
 import { QrScanner } from "@yudiel/react-qr-scanner";
+import { GridLoader } from "react-spinners";
 
 export default function Qr() {
+  const [prevResult, setPrevResult] = useState(null);
+  const handleSuccess = (result) => {
+    if (prevResult === null || prevResult !== result) {
+      setPrevResult(result);
+      alert(prevResult);
+      // mutate 전송 react-query
+    }
+  };
   return (
     <Layout isHeader isFooter>
       <div className="w-full h-full flex items-center">
-        <QrScanner
-          onDecode={(result) => console.log(result)}
-          onError={(error) => console.log(error?.message)}
-        />
+        {/* isLoading = true */}
+        {false ? (
+          <div className="w-full flex justify-center">
+            <GridLoader />
+          </div>
+        ) : (
+          <QrScanner
+            onDecode={handleSuccess}
+            onError={(error) => console.log(error?.message)}
+            constraints={{ facingMode: "environment" }}
+            scanDelay={500}
+          />
+        )}
       </div>
     </Layout>
   );
